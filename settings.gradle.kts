5cc7a4b58c20858fc41b44b53fd69ce4fb395059
Fff-1110 <1165578591@qq.com>
2026-08-08 12:47:40 +0800

    Add project skeleton: settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+}
+rootProject.name = "AI-Live-Overflow"
+include(':app')
