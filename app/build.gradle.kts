c806f1e78fa896850f9cb434911b436ed392788e
Fff-1110 <1165578591@qq.com>
2026-08-08 14:33:38 +0800

    Add repositories to app build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,11 @@ android {
     }
 }
 
+repositories {
+    google()
+    mavenCentral()
+}
+
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
